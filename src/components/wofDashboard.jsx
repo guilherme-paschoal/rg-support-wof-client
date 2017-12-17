8a4028f26145675d8a418d6a20b972d2c37a5c83
guilherme-paschoal <guilherme@hgdata.com>
2017-12-17 19:36:54 -0200

    Added bootstrap and prettified

--- src/components/wofDashboard.jsx
+++ src/components/wofDashboard.jsx
@@ -1,5 +1,8 @@
+
 import React, { Component } from 'react';
 import WofDashboardEngineer from './wofDashboardEngineer'
+import { Button, Col, Jumbotron, Grid, Row} from 'react-bootstrap';
+
 const R = require('ramda');
 
 class WofDashboard extends Component {
@@ -19,10 +22,23 @@ class WofDashboard extends Component {
   }
   render() {
     return (
-      <div>
-        <button onClick={ this.spin } > Spin </button>
-        { !R.isEmpty(this.props.people) && R.map(this.renderEngineer, this.props.people) }
-      </div>
+      <Grid>
+        <Col md={1}>
+        </Col>
+        <Col md={10}>
+          <Jumbotron>
+            <h1>The Support Wheel of Fate</h1>
+            <p> Welcome to the support wheel of fate! Click the button below to get the Engineers that are going to work today </p>
+            <p>
+              <Button onClick={ this.spin } bsStyle="primary" > Spin the Wheel </Button>
+            </p>
+          </Jumbotron>
+          { !R.isEmpty(this.props.people) && R.map(this.renderEngineer, this.props.people) }
+
+        </Col>
+        <Col md={1}>
+        </Col>
+      </Grid>
     );
   }
 }
--- src/components/wofDashboardEngineer.jsx
+++ src/components/wofDashboardEngineer.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Panel } from 'react-bootstrap';
 
 class WofDashboardEngineer extends Component {
   render() {
     return (
       <div>
-        {this.props.name}
+        <Panel header="First/Second/Third Shift">
+          {this.props.name}
+        </Panel>
       </div>
     );
   }
